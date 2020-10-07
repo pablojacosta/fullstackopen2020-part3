a93371ef27c943cfc52735a6f553cf019f449a5f
Pablo Acosta <sonlosdias@gmail.com>
2020-10-07 09:41:39 -0300

    uibuild

---
+++ build/precache-manifest.d4ed0bf796656ce7a8ce033e7dd45e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d9c1270f984a74062058deaa973cca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8773fa10888836c970d9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1601f8c01bf1a5ae8b95",
+    "url": "/static/js/2.081a6935.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.081a6935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8773fa10888836c970d9",
+    "url": "/static/js/main.0c6b3227.chunk.js"
+  },
+  {
+    "revision": "7eae4c9605f85aa4a617",
+    "url": "/static/js/runtime-main.152c13c9.js"
+  }
+]);
